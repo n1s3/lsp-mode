f668ec804f5b96f38ce44b229dcaada60867c020
Theodore Willows <theodoregx@gmail.com>
2024-01-02 14:37:34 -0500

    Update lsp-javascript.el

--- clients/lsp-javascript.el
+++ clients/lsp-javascript.el
@@ -800,7 +800,7 @@ name (e.g. `data' variable passed as `data' parameter)."
      (f-exists? (lsp-clients-typescript-project-ts-server-path)))
     (lsp-clients-typescript-project-ts-server-path))
    (t
-    (lsp-package-path 'typescript))))
+    (f-join (f-parent (f-parent (lsp-package-path 'typescript))) "lib" "node_modules" "typescript" "lib"))))
 
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-stdio-connection (lambda ()
